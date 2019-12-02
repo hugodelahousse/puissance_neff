ce4599074faa6ac24b3186cae6a0af1e1bd12611
mathieu-valade <valade.mathieu@gmail.com>
2019-12-02 15:48:23 +0100

    Theme: Add default dark theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import NavigationBar from "./views/navigationbar/NavigationBar";
+import NavigationBar from './views/navigationbar/NavigationBar';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <NavigationBar/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavigationBar />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
