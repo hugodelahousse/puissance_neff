14d7a85b98fcca58261ba871b2e327d9ae295978
mathieu-valade <valade.mathieu@gmail.com>
2019-12-02 17:50:03 +0100

    Route: Add Routes

--- frontend/src/views/navigationbar/NavigationBar.tsx
+++ frontend/src/views/navigationbar/NavigationBar.tsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import GamepadIcon from "@material-ui/icons/Gamepad";
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import GamepadIcon from '@material-ui/icons/Gamepad';
 
-const NavigationBar: React.FC = () => {
-    return (
-        <div className="Navigationbar">
-            <AppBar position="static">
-                <Toolbar>
-                    <GamepadIcon/>
-                    <Button color="inherit">Play</Button>
-                    <Button color="inherit">Leaderboards</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
-};
+function NavigationBar() {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <GamepadIcon />
+          <Button color="inherit">Play</Button>
+          <Button color="inherit">Leaderboards</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
 
 export default NavigationBar;
